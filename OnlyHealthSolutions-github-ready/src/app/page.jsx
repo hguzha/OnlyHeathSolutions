46146debd818474225c1ab65283c6cb5486c1921
hguzha <hguzha@gmail.com>
2026-03-04 15:29:17 -0500

    Update logo image sizes in page.jsx

--- OnlyHealthSolutions-github-ready/src/app/page.jsx
+++ OnlyHealthSolutions-github-ready/src/app/page.jsx
@@ -513,7 +513,7 @@ export default function OnlyHealthSolutionsSite() {
                 src={brand.logo1x}
                 srcSet={`${brand.logo1x} 1x, ${brand.logo2x} 2x`}
                 alt={`${brand.name} logo`}
-                className="h-28 sm:h-32 md:h-38 w-auto"
+                className="h-40 sm:h-44 md:h-50 w-auto"
                 loading="eager"
                 decoding="async"
               />
