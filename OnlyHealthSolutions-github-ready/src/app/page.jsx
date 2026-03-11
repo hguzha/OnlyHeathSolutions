4955a6eb0a27b462a05a8b5300a4fd4237235724
hguzha <hguzha@gmail.com>
2026-03-11 17:30:39 -0400

    Remove unused Button closing tag in page.jsx

--- OnlyHealthSolutions-github-ready/src/app/page.jsx
+++ OnlyHealthSolutions-github-ready/src/app/page.jsx
@@ -35,7 +35,6 @@ export default function HomePage() {
                   <Phone size={16} />
                   {brand.phoneDisplay}
                 </a>
-          </Button>
               </div>
             </div>
 
